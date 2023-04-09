fbb3015e332769da4b7654fe625a8e295105a9c7
Ivelin Iliev <73820008+ivooo369@users.noreply.github.com>
2023-04-09 22:12:03 +0300

    Add a library log to count total books and pages and read/unread books

--- app.js
+++ app.js
@@ -11,8 +11,16 @@ const cards = document.querySelector('.cards');
 const buttonToggle = document.querySelector('#btn-toggle');
 const buttonRemove = document.querySelector('.btn-remove');
 const msgExistingBook = document.querySelector('#msg-existing-book');
+const totalBooksCounter = document.querySelector('#total-books-counter');
+const totalPagesCounter = document.querySelector('#total-pages-counter');
+const readBooksCounter = document.querySelector('#read-books-counter');
+const unreadBooksCounter = document.querySelector('#unread-books-counter');
 
 const myLibrary = [];
+let totalBooks = 0;
+let totalPages = 0;
+let readBooks = 0;
+let unreadBooks = 0;
 class Book {
     constructor(title, author, pages, isRead) {
         this.title = title;
@@ -60,19 +68,49 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+function updateLibraryLog() {
+    totalBooksCounter.textContent = totalBooks;
+    totalPagesCounter.textContent = totalPages;
+    readBooksCounter.textContent = readBooks;
+    unreadBooksCounter.textContent = unreadBooks;
+}
+
 function createNewBook() {
     const newBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value, checkbox.checked);
     if (checkForDuplicateTitle(newBook)) {
         addBookToLibrary(newBook);
+        totalBooks++;
+        totalPages += Number(inputPages.value);
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = newBook.isRead;
+        checkbox.classList.add('checkbox');
+        checkbox.addEventListener('change', () => {
+            if (checkbox.checked) {
+                newBook.isRead = true;
+                readBooks++;
+                if (unreadBooks > 0) {
+                    unreadBooks--;
+                }
+            } else {
+                newBook.isRead = false;
+                unreadBooks++;
+                if (readBooks > 0) {
+                    readBooks--;
+                }
+            }
+            updateLibraryLog();
+        });
         const bookCard = document.createElement('div');
         bookCard.classList.add('book-card');
         bookCard.innerHTML = `<p>Title: ${inputTitle.value}</p>
-            <p>Author: ${inputAuthor.value}</p>
-            <p>Pages: ${inputPages.value}</p>
-            <div class="read-or-not">
-            <span>Mark as read?</span>
-                <input type="checkbox" class="checkbox" ${checkbox.checked ? 'checked' : ''}>
-            </div>`;
+          <p>Author: ${inputAuthor.value}</p>
+          <p>Pages: ${inputPages.value}</p>
+          <div class="read-or-not">
+          <span>Mark as read?</span>
+          </div>`;
+        const readOrNot = bookCard.querySelector('.read-or-not');
+        readOrNot.appendChild(checkbox);
         cards.appendChild(bookCard);
         const buttonRemove = document.createElement('button');
         buttonRemove.classList.add('btn-remove');
@@ -80,12 +118,27 @@ function createNewBook() {
         bookCard.appendChild(buttonRemove);
         buttonRemove.addEventListener('click', removeBook);
         function removeBook() {
+            const pagesToRemove = Number(bookCard.querySelector('p:nth-of-type(3)').textContent.split(' ')[1]);
             bookCard.remove();
             const index = myLibrary.indexOf(newBook);
             if (index !== -1) {
                 myLibrary.splice(index, 1);
             }
+            totalBooks--;
+            totalPages -= pagesToRemove;
+            if (newBook.isRead && readBooks > 0) {
+                readBooks--;
+            } else if (!newBook.isRead && unreadBooks > 0) {
+                unreadBooks--;
+            }
+            updateLibraryLog();
         }
+        if (newBook.isRead) {
+            readBooks++;
+        } else {
+            unreadBooks++;
+        }
+        updateLibraryLog();
         closePopup();
     }
 }
